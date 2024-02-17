de9c1cdad8442c4fbaca915530eb288bd32dc45f
Lance Hart <shipsimfan@hotmail.com>
2024-02-17 13:00:24 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+//! Raw bindings for `liburing`
+
+#![deny(missing_docs)]
+#![deny(rustdoc::private_intra_doc_links)]
+#![deny(rustdoc::unescaped_backticks)]
+#![deny(rustdoc::redundant_explicit_links)]
+#![warn(rustdoc::broken_intra_doc_links)]
