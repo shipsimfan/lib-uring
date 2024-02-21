9cf7bc5ecc00c931d1a9405a35829bed4f4933c5
Lance Hart <shipsimfan@hotmail.com>
2024-02-21 13:22:18 -0500

    Updated io_uring_cq_ready

--- src/functions/io_uring_cq_ready.rs
+++ src/functions/io_uring_cq_ready.rs
@@ -1,15 +1,17 @@
 use crate::io_uring;
-use std::ffi::c_uint;
+use std::{
+    ffi::c_uint,
+    sync::atomic::{AtomicU32, Ordering},
+};
 
-#[link(name = "uring")]
-extern "C" {
-    /// Returns number of unconsumed ready entries in the CQ ring
-    ///
-    /// # Description
-    /// The [`io_uring_cq_ready`] function returns the number of unconsumed entries that are ready
-    /// belonging to the `ring` parameter.
-    ///
-    /// # Return Value
-    /// Returns the number of unconsumed ready entries in the CQ ring.
-    pub fn io_uring_cq_ready(ring: *const io_uring) -> c_uint;
+/// Returns number of unconsumed ready entries in the CQ ring
+///
+/// # Description
+/// The [`io_uring_cq_ready`] function returns the number of unconsumed entries that are ready
+/// belonging to the `ring` parameter.
+///
+/// # Return Value
+/// Returns the number of unconsumed ready entries in the CQ ring.
+pub unsafe fn io_uring_cq_ready(ring: *const io_uring) -> c_uint {
+    AtomicU32::from_ptr((*ring).cq.ktail).load(Ordering::Acquire) - *(*ring).cq.khead
 }
